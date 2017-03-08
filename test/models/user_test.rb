f6f9bcfab5d6ec168b34b9d3df655e87425c7447
UgoMare <ugo.mare.epitech@gmail.com>
2017-03-08 11:00:11 +0100

    travis

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,5 +5,8 @@ class UserTest < ActiveSupport::TestCase
   test "full_name returns the capitalized fist name and last name" do
     user = User.new(first_name: "john", last_name: "lennon")
     assert_equal "John Lennon", user.full_name
+
+    user = User.new(first_name: "RINGO", last_name: "STARR")
+    assert_equal "Ringo Starr", user.full_name
   end
 end

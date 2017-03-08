e6250aaccfa3c2ea5d6c5f5cc369d52a5853704c
UgoMare <ugo.mare.epitech@gmail.com>
2017-03-08 10:52:35 +0100

    Add new product form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'products/new'
+
   devise_for :users
   root to: 'products#index'
+  resources :products, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
